039dfbaf1fd13f2b7ea120cb5cd498dc09791d6a
yuhangster <lucivoyant@gmail.com>
2022-05-14 16:25:50 -0400

    updated with dde23 NW
    1st version not working dde23

--- ddefun.m
+++ ddefun.m
@@ -1,31 +1,31 @@
 %% dydt
 % x is a 8 by 1 vector which is x1 ... x8 
-function dydt = odefun(t, x, L, c, H_e, Tao_e, H_i, Tao_i, gamma, AF,AB,AL,delta)
+function dydt = ddefun(t, x, Z, L, c, H_e, Tao_e,H_i, Tao_i, gamma,AF,AB,AL)
     t_imp = 10;
-    t_end = 800;
+    t_end = 600;
     u = interpU(t_imp, t_end, t);
     
     dydt = zeros(8*L,1);
     for i = 1:L
-        dydt(1+8*(i-1):8*i,1) = column(i, t, x, u, c, H_e, Tao_e, H_i, Tao_i, gamma, AF,AB,AL,delta);
+        dydt(1+8*(i-1):8*i,1) = column(i, t, x, Z, u, c, H_e, Tao_e, H_i, Tao_i, gamma, AF,AB,AL);
     end
 end
 
 % Ode funtcion for one column (8 by 1)
-function Ode = column(i, t, x, u, c, H_e, Tao_e, H_i, Tao_i, gamma, AF,AB,AL,delta)
+function Ode = column(i, t, x, Z, u, c, H_e, Tao_e, H_i, Tao_i, gamma, AF,AB,AL)
     k = H_e/Tao_e;
     y = x(2+8*(i-1))-x(3+8*(i-1));
-    
+    %set up the equation
     dydt = zeros(8,1);
     dydt(1) = x(4+8*(i-1));
-    if i == 1 && length(x) > 8 
-        yj = x(10) - x(11);
-        dydt(4) = k*( AF(1,2)*S(yj*(t-delta))+c*u+gamma(1)*(S(y)) )-2*x(4+8*(i-1))/Tao_e-x(1+8*(i-1))/(Tao_e^2);
-    elseif i == 2
-        yj = x(2) - x(3);
-        dydt(4) = k*( AF(2,1)*S(yj*(t-delta))+gamma(1)*(S(y)) )-2*x(4+8*(i-1))/Tao_e-x(1+8*(i-1))/(Tao_e^2);
+    if i == 2
+        %the yj is the time delayed version 
+        %need to to be changed
+        xlag = Z(:,1);
+        yj = xlag(2) - xlag(3);
+        dydt(4) = k*( AF(2,1)*S(yj)+gamma(1)*(S(y)) )-2*x(4+8*(i-1))/Tao_e-x(1+8*(i-1))/(Tao_e^2);
     else
-        dydt(4) = k*(c*u+gamma(1)*(S(y)) )-2*x(4+8*(i-1))/Tao_e-x(1+8*(i-1))/(Tao_e^2);
+        dydt(4) = k*(c*u+gamma(1)*(S(y)))-2*x(4+8*(i-1))/Tao_e-x(1+8*(i-1))/(Tao_e^2);
     end
     dydt(2) = x(5+8*(i-1));
     dydt(5) = k*( gamma(2)*(S(x(1+8*(i-1)))) )-2*x(5+8*(i-1))/Tao_e-x(2+8*(i-1))/(Tao_e^2);
---
+++ history.m
@@ -0,0 +1,5 @@
+%solution history is the solution for times t≤t0
+%need to be changed 
+function s = history(t, L)
+    s = ones(8*L,1);
+end
