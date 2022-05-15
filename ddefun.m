6a60fa94ef63a3ce878cb3b5c3545891884bc29e
yuhangster <lucivoyant@gmail.com>
2022-05-15 12:57:00 -0400

    not functioning matrix multiplicaion
    matrix multiplication does not work

--- ddefun.m
+++ ddefun.m
@@ -7,26 +7,30 @@ function dydt = ddefun(t, x, Z, L, c, H_e, Tao_e,H_i, Tao_i, gamma,AF,AB,AL)
     
     dydt = zeros(8*L,1);
     for i = 1:L
-        dydt(1+8*(i-1):8*i,1) = column(i, t, x, Z, u, c, H_e, Tao_e, H_i, Tao_i, gamma, AF,AB,AL);
+        dydt(1+8*(i-1):8*i,1) = column(L, i, t, x, Z, u, c, H_e, Tao_e, H_i, Tao_i, gamma, AF,AB,AL);
     end
 end
 
 % Ode funtcion for one column (8 by 1)
-function Ode = column(i, t, x, Z, u, c, H_e, Tao_e, H_i, Tao_i, gamma, AF,AB,AL)
+function Ode = column(L, i, t, x, Z, u, c, H_e, Tao_e, H_i, Tao_i, gamma, AF,AB,AL)
+    %simplify terms
     k = H_e/Tao_e;
     y = x(2+8*(i-1))-x(3+8*(i-1));
     %set up the equation
     dydt = zeros(8,1);
     dydt(1) = x(4+8*(i-1));
-    if i == 2
-        %the yj is the time delayed version 
-        %need to to be changed
-        xlag = Z(:,1);
-        yj = xlag(2) - xlag(3);
-        dydt(4) = k*( AF(2,1)*S(yj)+gamma(1)*(S(y)) )-2*x(4+8*(i-1))/Tao_e-x(1+8*(i-1))/(Tao_e^2);
-    else
-        dydt(4) = k*(c*u+gamma(1)*(S(y)))-2*x(4+8*(i-1))/Tao_e-x(1+8*(i-1))/(Tao_e^2);
-    end
+    %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%     xlag1 = Z(:,1);
+%     S_yj = zeros(L,1); 
+%     for k = 1:L
+%         tmp = xlag1(2+8*(k-1)) - xlag1(3+8*(k-1));
+%         S_yj(k,1) = S(tmp);
+%     end
+    %dydt(4) = k*( AF(i,1)*S_yj(1)+c(1,i)*u+gamma(1)*(S(y)))-2*x(4+8*(i-1))/Tao_e-x(1+8*(i-1))/(Tao_e^2);
+    xlag = Z(:,1);
+    yj = xlag(2) - xlag(3);
+    dydt(4) = k*( AF(i,1)*S(yj)+c(1,i)*u+gamma(1)*(S(y)))-2*x(4+8*(i-1))/Tao_e-x(1+8*(i-1))/(Tao_e^2);
+    %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     dydt(2) = x(5+8*(i-1));
     dydt(5) = k*( gamma(2)*(S(x(1+8*(i-1)))) )-2*x(5+8*(i-1))/Tao_e-x(2+8*(i-1))/(Tao_e^2);
     dydt(3) = x(6+8*(i-1));
--- nonlinear.m
+++ nonlinear.m
@@ -13,41 +13,86 @@ gamma2 = 40;
 gamma3 = 12;
 gamma4 = 12;
 gamma = [gamma1 gamma2 gamma3 gamma4];
-c = [1 1000 1e6 1e9];
+
 delta = 10; % ms
 opts = odeset('MaxStep',1);
 AB=0;
 AL=0;
 
 %% part 1
-L = 1; %L is the total number of columns
-AF = 0;
-AB=0;
-AL=0;
-tspan = [0 400];
-lags = [delta delta];
-x0 = zeros(1,8*L);
-%plot and calculate the terms
-figure;
-for i = 1:length(c)
-    sol = dde23(@(t,x,Z) ddefun(t, x, Z, L, c(i), H_e, Tao_e,H_i, Tao_i, gamma,AF,AB,AL), lags, @(t) history(t,L), tspan, opts);
-    subplot(2,2,i);
-    plot(sol.x, (sol.y(2,:) - sol.y(3,:)));
-    title('c = ', c(i));
-end
+% L = 1; %L is the total number of columns
+% c = zeros(4,L);
+% for tp = 1:4
+%     c(tp,1) = 1000^(tp-1);
+% end
+% 
+% AF = 0;
+% AB=0;
+% AL=0;
+% tspan = [0 400];
+% lags = [delta];
+% x0 = zeros(1,8*L);
+% %plot and calculate the terms
+% figure;
+% for i = 1:length(c)
+%     sol = dde23(@(t,x,Z) ddefun(t, x, Z, L, c(i,:), H_e, Tao_e,H_i, Tao_i, gamma,AF,AB,AL), lags, @(t) history(t,L), tspan, opts);
+%     subplot(2,2,i);
+%     plot(sol.x, (sol.y(2,:) - sol.y(3,:)));
+%     title('c = ', c(i));
+% end
 
 %% part 2
 L = 2; 
+c = zeros(4,L);
+for tp = 1:4
+    c(tp,1) = 1000^(tp-1);
+end
 x0 = zeros(1,8*L);
 tspan = [0 600];
 %plot and calculate the terms
 figure;
 AF = [0 0; 1 0];
-lags = [delta delta];
+lags = [delta];
 for i = 1:4
     AF = AF*10;
-    sol = dde23(@(t,x,Z) ddefun(t, x, Z, L, c(2), H_e, Tao_e,H_i, Tao_i, gamma,AF,AB,AL), lags, @(t) history(t,L), tspan, opts);
+    sol = dde23(@(t,x,Z) ddefun(t, x, Z, L, c(2,:), H_e, Tao_e,H_i, Tao_i, gamma,AF,AB,AL), lags, @(t) history(t,L), tspan, opts);
     subplot(2,2,i);
     plot(sol.x, (sol.y(2,:) - sol.y(3,:)),'r', sol.x, (sol.y(10,:) - sol.y(11,:)),'k');
     title('foward = ', AF(2,1));
 end
+
+%% part 3 5 consective AF only structure
+% L = 5; 
+% x0 = zeros(1,8*L);
+% tspan = [0 600];
+% %plot and calculate the terms
+% AF = [0 0; 40 0];
+% lags = [delta ];
+% sol = dde23(@(t,x,Z) ddefun(t, x, Z, L, c(2), H_e, Tao_e,H_i, Tao_i, gamma,AF,AB,AL), lags, @(t) history(t,L), tspan, opts);
+% figure;
+% for i =1:L
+%     subplot(1,5,i);
+%     plot(sol.x, (sol.y(2+8*(i-1),:) - sol.y(3+8*(i-1),:)));
+%     title('Area ', i);
+% end
+
+
+%% part 4 Af and Ab
+% L = 2;
+% x0 = zeros(1,8*L);
+% tspan = [0 1000];
+% %plot and calculate the terms
+% figure;
+% AF = [0 0; 40 0];
+% AB = zeros(2,2,4);
+% AB(:,:,1) = [0 1; 0 0];
+% AB(:,:,2) = [0 10; 0 0];
+% AB(:,:,3) = [0 25; 0 0];
+% AB(:,:,4) = [0 50; 0 0];
+% lags = [delta delta];
+% for i  = 1:4
+%     sol = dde23(@(t,x,Z) ddefun(t, x, Z, L, c(2), H_e, Tao_e,H_i, Tao_i, gamma,AF,AB,AL), lags, @(t) history(t,L), tspan, opts);
+%     subplot(2,2,i);
+%     plot(sol.x, (sol.y(2,:) - sol.y(3,:)),'r', sol.x, (sol.y(10,:) - sol.y(11,:)),'k');
+%     title('foward = ', AF(2,1));
+% end
