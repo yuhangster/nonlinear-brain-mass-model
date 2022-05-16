e332138089b6e985a6692ee8ee7cddcd22733583
yuhangster <lucivoyant@gmail.com>
2022-05-16 14:39:04 -0400

    deleted data generate code

--- history.m
+++
@@ -1,5 +0,0 @@
-%solution history is the solution for times t≤t0
-%need to be changed 
-function s = history(t, L)
-    s = zeros(8*L,1);
-end
--- initC.m
+++
@@ -1,6 +0,0 @@
-function c = initC(L)
-    c = zeros(4,L);
-    for tp = 1:4
-        c(tp,1) = 1000^(tp-1);
-    end
-end
